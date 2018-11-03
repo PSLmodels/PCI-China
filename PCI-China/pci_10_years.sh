7c8fe21bf0877903af6148476ff95e5d0cab9bb4
Weifeng Zhong <weifeng@weifengzhong.com>
2018-11-03 18:51:39 -0400

    change shells from quarterly to monthly

--- PCI-China/pci_10_years.sh
+++ PCI-China/pci_10_years.sh
@@ -2,11 +2,11 @@ for j in `seq 1 5`
 do
     for y in `seq 1956 2018` 
     do
-        for q in `seq 1 4` 
+        for m in `seq 1 12` 
         do
             for i in `seq 1 4` 
             do
-                python pci.py --model="window_10_years" --year=$y --quarter=$q --gpu=0 --iterator=$i
+                python pci.py --model="window_10_years" --year=$y --month=$m --gpu=0 --iterator=$i
             done
         done
     done
