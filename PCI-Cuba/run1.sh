f19980231e05feb6abcfe9f84e918afe3560a94e
Weifeng Zhong <weifeng@weifengzhong.com>
2018-12-13 01:51:59 -0500

    added scripts for running models

---
+++ PCI-Cuba/run1.sh
@@ -0,0 +1,14 @@
+for j in `seq 1 1000` 
+do
+    for y in `seq 1970 1970` 
+    do
+        for q in 1
+        do
+            for i in `seq 1 8` 
+            do
+                python pci.py --model="window_5_years_quarterly" --year=$y --month=$q --gpu=1 --iterator=$i
+            done
+        done
+    done
+done
+# python pci.py --model="window_5_years" --year=2010 --month=1 --gpu=0 --iterator=1
