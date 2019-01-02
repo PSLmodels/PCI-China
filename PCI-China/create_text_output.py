0acf480b721f9c17e3da6848410ec0700faf8c97
Tsz Kin Chan <ctszkin@gmail.com>
2019-01-01 20:27:39 -0500

    Update code that generates text output

--- PCI-China/create_text_output.py
+++ PCI-China/create_text_output.py
@@ -5,8 +5,8 @@ from src.pci_model import *
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", help="Model name: window_5_years_quarterly, window_10_years_quarterly")
-    parser.add_argument("--from_year", help="From year", default=1950, type=int)
-    parser.add_argument("--to_year", help="To year", default=2018, type=int)
+    parser.add_argument("--year", help="year", default=1950, type=int)
+    parser.add_argument("--quarter", help="quarter", default=1, type=int)
     parser.add_argument("--gpu", help="To year", default="0")
 
     args = parser.parse_args()
@@ -14,15 +14,18 @@ if __name__ == "__main__":
         print('Error: model must be "window_5_years_quarterly" or "window_10_years_quarterly"' )
         sys.exit(1)
 
-    path = "./models/" + args.model + "/"
+    path = "./models/" + args.model + "/" + str(args.year) + "_M" + str(args.quarter) + "/" 
+    
+    if os.path.exists(path + "model.hd5"):
+        create_text_output(path, args.gpu)
 
 
-    for y in range(args.from_year, args.to_year+1):
-        for q in [1, 4, 7, 10]:
-            print(str(y) + "_M" + str(q))
-            cur_path = path + str(y) + "_M" + str(q) + "/" 
-            if os.path.exists(cur_path + "model.hd5"):
-                create_text_output(cur_path, args.gpu)
+    # for y in range(args.from_year, args.to_year+1):
+    #     for q in [1, 4, 7, 10]:
+    #         print(str(y) + "_M" + str(q))
+    #         cur_path = path + str(y) + "_M" + str(q) + "/" 
+    #         if os.path.exists(cur_path + "model.hd5"):
+    #             create_text_output(cur_path, args.gpu)
 
                     
 
