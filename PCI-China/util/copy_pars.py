3a714f7955f3a144dda95885b140b62d7211b583
Tsz Kin Chan <ctszkin@gmail.com>
2019-01-06 22:54:38 -0500

    fix bugs

--- PCI-China/util/copy_pars.py
+++ PCI-China/util/copy_pars.py
@@ -1,13 +1,14 @@
 import os
 from shutil import copyfile
+import shutil
 import argparse
 
 def copy_pars(in_path, out_path):
     for i in range(1950,2019):
         for m in [1, 4, 7, 10]:
             out_path2 = out_path + str(i) + "_M" + str(m) 
-            in_path2  = in_path + str(i) + "_M" +str()
-
+            in_path2  = in_path + str(i) + "_M" +str(m)
+            print(in_path2)
             if os.path.exists(in_path2 + "/best_pars.pkl"):
                 if not os.path.exists(out_path2):
                     os.makedirs(out_path2)
@@ -19,3 +20,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--in_path", help="input path")
     parser.add_argument("--out_path", help="output path")
+    args = parser.parse_args()
+
+    copy_pars(args.in_path, args.out_path)
