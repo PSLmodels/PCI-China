1334b6caad3755ba9ae125562642a936d38deed6
Tsz Kin Chan <ctszkin@gmail.com>
2019-01-06 22:33:54 -0500

    Create figures for 10 years quarterly

--- PCI-China/generate_figures.r
+++ PCI-China/generate_figures.r
@@ -23,7 +23,7 @@ hh = 4
 ######
 
 data_5y = prepare_data("window_5_years_quarterly", .ROOT )
-# data_10y = prepare_data("window_10_years", .ROOT)
+data_10y = prepare_data("window_10_years_quarterly", .ROOT)
 
 ggsave(file.path(.OUTPUT,"diff_wo_events_5y.png"), plot = plot_pci(data_5y,event=FALSE,abs=FALSE) , width=hh*2.4 ,height=1.5*hh)
 ggsave(file.path(.OUTPUT,"pci_wo_events_5y.png"), plot = plot_pci(data_5y,event=FALSE,abs=TRUE), width=hh*2.4 ,height=1.5*hh)
@@ -31,14 +31,14 @@ ggsave(file.path(.OUTPUT,"diff_with_events_5y.png"), plot = plot_pci(data_5y,eve
 ggsave(file.path(.OUTPUT,"pci_with_events_5y.png"), plot = plot_pci(data_5y,event=TRUE,abs=TRUE), width=hh*2.4 ,height=1.5*hh)
 ggsave(file.path(.OUTPUT,"F1_two_curves_5y.png"), plot = plot_F1(data_5y), width=hh*2.4 ,height=1.5*hh)
 
-# ggsave(file.path(.OUTPUT,"diff_wo_events_10y.png"), plot = plot_pci(data_10y,event=FALSE,abs=FALSE) , width=hh*2.4 ,height=1.5*hh)
-# ggsave(file.path(.OUTPUT,"pci_wo_events_10y.png"), plot = plot_pci(data_10y,event=FALSE,abs=TRUE), width=hh*2.4 ,height=1.5*hh)
-# ggsave(file.path(.OUTPUT,"diff_with_events_10y.png"), plot = plot_pci(data_10y,event=TRUE,abs=FALSE), width=hh*2.4 ,height=1.5*hh)
-# ggsave(file.path(.OUTPUT,"pci_with_events_10y.png"), plot = plot_pci(data_10y,event=TRUE,abs=TRUE), width=hh*2.4 ,height=1.5*hh)
-# ggsave(file.path(.OUTPUT,"F1_two_curves_10y.png"), plot = plot_F1(data_10y), width=hh*2.4 ,height=1.5*hh)
-# 
-# ggsave(file.path(.OUTPUT,"F1_testing_5yrs_vs_10yrs.png"), plot = plot_compare(data_5y,data_10y,"F1"), width=hh*2.4 ,height=1.5*hh)
-# ggsave(file.path(.OUTPUT,"pci_wo_events_5yrs_vs_10yrs.png"), plot = plot_compare(data_5y,data_10y,"PCI"), width=hh*2.4 ,height=1.5*hh)
+ggsave(file.path(.OUTPUT,"diff_wo_events_10y.png"), plot = plot_pci(data_10y,event=FALSE,abs=FALSE) , width=hh*2.4 ,height=1.5*hh)
+ggsave(file.path(.OUTPUT,"pci_wo_events_10y.png"), plot = plot_pci(data_10y,event=FALSE,abs=TRUE), width=hh*2.4 ,height=1.5*hh)
+ggsave(file.path(.OUTPUT,"diff_with_events_10y.png"), plot = plot_pci(data_10y,event=TRUE,abs=FALSE), width=hh*2.4 ,height=1.5*hh)
+ggsave(file.path(.OUTPUT,"pci_with_events_10y.png"), plot = plot_pci(data_10y,event=TRUE,abs=TRUE), width=hh*2.4 ,height=1.5*hh)
+ggsave(file.path(.OUTPUT,"F1_two_curves_10y.png"), plot = plot_F1(data_10y), width=hh*2.4 ,height=1.5*hh)
+
+ggsave(file.path(.OUTPUT,"F1_testing_5yrs_vs_10yrs.png"), plot = plot_compare(data_5y,data_10y,"F1"), width=hh*2.4 ,height=1.5*hh)
+ggsave(file.path(.OUTPUT,"pci_wo_events_5yrs_vs_10yrs.png"), plot = plot_compare(data_5y,data_10y,"PCI"), width=hh*2.4 ,height=1.5*hh)
 
 
 
