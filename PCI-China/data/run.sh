211afd5a65278b81d49af6496efda518b790f73e
Tsz Kin Chan <ctszkin@gmail.com>
2019-04-11 00:24:22 -0400

    Adding 2019Q1

--- PCI-China/data/run.sh
+++ PCI-China/data/run.sh
@@ -4,7 +4,8 @@
 # python proc_pd.py --input="Input/pd/pd_2000_2017.pkl" --create=0 --seed=5 --k_fold=10 --output="Output/database.db"
 # python proc_pd.py --input="Input/pd/pd_2018_Q1_to_Q3.pkl" --create=0 --seed=3 --k_fold=10 --output="Output/database.db"
 # python proc_pd.py --input="Input/pd/pd_2018_10.pkl" --create=0 --seed=2 --k_fold=10 --output="Output/database.db"
-python proc_pd.py --input="Input/pd/pd_2018_11.pkl" --create=0 --seed=201811 --k_fold=10 --output="Output/database.db"
-python proc_pd.py --input="Input/pd/pd_2018_12.pkl" --create=0 --seed=201812 --k_fold=10 --output="Output/database.db"
+# python proc_pd.py --input="Input/pd/pd_2018_11.pkl" --create=0 --seed=201811 --k_fold=10 --output="Output/database.db"
+# python proc_pd.py --input="Input/pd/pd_2018_12.pkl" --create=0 --seed=201812 --k_fold=10 --output="Output/database.db"
 
+python proc_pd.py --input="Input/pd/2019_Q1.pkl" --create=0 --seed=201901 --k_fold=10 --output="Output/database.db"
 
