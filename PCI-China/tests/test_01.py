271fb787103b70545de16a6678deaa24545411d5
Tsz Kin Chan <ctszkin@gmail.com>
2019-04-28 17:04:01 -0400

    tests

--- PCI-China/tests/test_01.py
+++ PCI-China/tests/test_01.py
@@ -1,8 +1,8 @@
-import pytest
-import sys
+import pytest, sys, os
 from src.data import *
 from src.figures import *
-import os 
+from src.utils import *
+
 def test_create_plotly():
     fig = create_plotly_figure(input = "./figures/pci.csv")
 
@@ -16,6 +16,18 @@ def test_proc_data():
         input_file = "./tests/Data/Input/embedding_example.txt",
         output_path = "./tests/Data/Output/"
     )
+
+    with open("./tests/Data/Output/embedding.pkl" , 'rb') as f:
+        embedding = pickle.load(f)
+        
+    words = list( embedding.keys())
+
+    example_data_2000 = gen_testing_data(words = words, from_year = 2000, to_year = 2010, type = 0, seed = 1 )
+    example_data_2000.to_pickle("./tests/Data/Output/2000_2010.pkl")
+
+    example_data_2011 = gen_testing_data(words = words, from_year = 2011, to_year = 2011, type = 1, seed = 2 )
+    example_data_2011.to_pickle("./tests/Data/Output/2011.pkl")
+
 
     proc_pd(
         input  = "./tests/Data/Output/2000_2010.pkl",
