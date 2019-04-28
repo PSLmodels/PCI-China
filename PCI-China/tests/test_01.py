2bae3e2835a6da561befcbd7280da69e6232ce6d
Tsz Kin Chan <ctszkin@gmail.com>
2019-04-28 16:04:46 -0400

    update tests

--- PCI-China/tests/test_01.py
+++ PCI-China/tests/test_01.py
@@ -2,30 +2,32 @@ import pytest
 import sys
 from src.data import *
 from src.figures import *
-
+import os 
 def test_create_plotly():
     fig = create_plotly_figure(input = "./figures/pci.csv")
 
 def test_proc_data():
+    print(os.getcwd())
+    
     proc_embedding(
-        input_file = "Tests/Data/Input/embedding_example.txt",
-        output_path = "Tests/Data/Output/"
+        input_file = "./Tests/Data/Input/embedding_example.txt",
+        output_path = "./Tests/Data/Output/"
     )
 
     proc_pd(
-        input  = "Tests/data/Output/2000_2010.pkl",
+        input  = "./Tests/data/Output/2000_2010.pkl",
         create = 0,
         seed   = 1,
         k_fold = 10,
-        output = "Tests/Data/Output/database.db",
-        embedding = "Tests/Data/Output/"
+        output = "./Tests/Data/Output/database.db",
+        embedding = "./Tests/Data/Output/"
     )
 
     proc_pd(
-        input  = "Tests/data/Output/2011.pkl",
+        input  = "./Tests/data/Output/2011.pkl",
         create = 0,
         seed   = 2,
         k_fold = 10,
-        output = "Tests/Data/Output/database.db",
-        embedding = "Tests/Data/Output/"
+        output = "./Tests/Data/Output/database.db",
+        embedding = "./Tests/Data/Output/"
     )
